57fe0c58eec57b333f2c8677c2d236d51a17c98c
SSBozzelli <andrewbozzelli@my.tridenttech.edu>
2014-05-01 14:12:54 -0400

    added a nav bar

--- javascripts/my.js
+++ javascripts/my.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("button").mouseenter(function(){
-        $("button").fadeTo("slow",1);
+    $(".submitButton").mouseenter(function(){
+        $(".submitButton").fadeTo("slow",1);
     });
-    $("button").mouseleave(function(){
-        $("button").fadeTo("slow",0.5);
+    $(".submitButton").mouseleave(function(){
+        $(".submitButton").fadeTo("slow",0.5);
     });
     
 });
