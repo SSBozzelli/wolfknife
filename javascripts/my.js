26d532f6a9d69b9cabc55b879796704cf4c0ee99
SSBozzelli <andrewbozzelli@my.tridenttech.edu>
2014-05-08 13:57:10 -0400

    Added functionality to the password text boxes.

--- javascripts/my.js
+++ javascripts/my.js
@@ -1,3 +1,26 @@
+function validatePass()
+	{
+
+	    var pass1 = document.getElementById('password');
+	    var pass2 = document.getElementById('password2');
+	        var message = document.getElementById('confirmMessage');
+	    var goodColor = "#2c9c49";
+	    var badColor = "#ff6666";
+	   
+	    if(pass1.value == pass2.value)
+	    {
+	       
+	        pass2.style.backgroundColor = goodColor;
+	        message.style.color = goodColor;
+	        message.innerHTML = "&#x2713 Passwords Match!";
+	    }else{
+	        
+	        pass2.style.backgroundColor = badColor;
+	        message.style.color = badColor;
+	        message.innerHTML = "&#x1f6ab Passwords Do Not Match!";
+	    }
+	}  
+
 $(document).ready(function(){
     $(".submitButton").mouseenter(function(){
         $(".submitButton").fadeTo("slow",1);
